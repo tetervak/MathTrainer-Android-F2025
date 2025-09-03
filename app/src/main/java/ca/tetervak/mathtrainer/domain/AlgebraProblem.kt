dc84443e87d324266656353bdecc31a3b7f58671
Alex Tetervak <tetervak@sheridancollege.ca>
2025-09-03 18:43:15 -0400

    added AlgebraProblem abstract class

---
+++ app/src/main/java/ca/tetervak/mathtrainer/domain/AlgebraProblem.kt
@@ -0,0 +1,24 @@
+package ca.tetervak.mathtrainer.domain
+
+import kotlin.math.abs
+
+abstract class AlgebraProblem: Problem {
+
+    protected abstract val answer: Int
+
+    override fun checkAnswer(userAnswer: String): ProblemGrade =
+        try {
+            val entered: Double = userAnswer.toDouble()
+            if (abs(answer - entered) <= TOLERANCE) {
+                ProblemGrade.RIGHT_ANSWER
+            } else {
+                ProblemGrade.WRONG_ANSWER
+            }
+        } catch (e: NumberFormatException) {
+            ProblemGrade.INVALID_INPUT
+        }
+
+    companion object {
+        const val TOLERANCE: Double = 0.000001
+    }
+}
