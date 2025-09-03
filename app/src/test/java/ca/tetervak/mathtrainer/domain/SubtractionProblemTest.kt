885c9ebd7676329f04438045e73361fc9fa3e658
Alex Tetervak <tetervak@sheridancollege.ca>
2025-09-03 19:49:07 -0400

    added SubtractionProblemTest

---
+++ app/src/test/java/ca/tetervak/mathtrainer/domain/SubtractionProblemTest.kt
@@ -0,0 +1,72 @@
+package ca.tetervak.mathtrainer.domain
+
+import org.junit.After
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+class SubtractionProblemTest {
+
+    var problem = SubtractionProblem(5,3)
+
+    @Before
+    fun setUp() {
+        println("--- testing case ---")
+        println("problem = $problem")
+    }
+
+    @After
+    fun tearDown() {
+        println("--- ------- ---- ---")
+    }
+
+    @Test
+    fun checkAnswer() {
+        println("problem.answer = ${problem.answer}")
+
+        println("problem.userAnswer = \"2\", grade = ${problem.checkAnswer("2")}")
+        assertEquals(
+            ProblemGrade.RIGHT_ANSWER,
+            problem.checkAnswer("2"))
+
+        println("problem.userAnswer = \"4\", grade = ${problem.checkAnswer("4")}")
+        assertEquals(
+            ProblemGrade.WRONG_ANSWER,
+            problem.checkAnswer("4"))
+
+        println("problem.userAnswer = \"whatever\", grade = ${problem.checkAnswer("whatever")}")
+        assertEquals(
+            ProblemGrade.INVALID_INPUT,
+            problem.checkAnswer("whatever"))
+
+        println("problem.userAnswer = \"2.0\", grade = ${problem.checkAnswer("2.0")}")
+        assertEquals(
+            ProblemGrade.RIGHT_ANSWER,
+            problem.checkAnswer("2.0"))
+
+        println("problem.userAnswer = \"2.1\", grade = ${problem.checkAnswer("2.1")}")
+        assertEquals(
+            ProblemGrade.WRONG_ANSWER,
+            problem.checkAnswer("2.1"))
+
+        println("problem.userAnswer = \"1.9\", grade = ${problem.checkAnswer("1.9")}")
+        assertEquals(
+            ProblemGrade.WRONG_ANSWER,
+            problem.checkAnswer("1.9"))
+    }
+
+    @Test
+    fun getAnswer() {
+        println("problem.answer = ${problem.answer}")
+        assertEquals(2, problem.answer)
+    }
+
+    @Test
+    fun getText() {
+        println("problem.text = \"${problem.text}\"")
+        assertEquals("5 - 3 = ?", problem.text)
+    }
+
+
+
+}
