bad59b4a0b0e4eacb38fa3425466207617d599b4
Alex Tetervak <tetervak@sheridancollege.ca>
2025-10-02 11:49:00 -0400

    deleted an unused variable

--- app/src/main/java/ca/tetervak/mathtrainer/ui/details/ProblemDetailsUiState.kt
+++ app/src/main/java/ca/tetervak/mathtrainer/ui/details/ProblemDetailsUiState.kt
@@ -3,6 +3,5 @@ package ca.tetervak.mathtrainer.ui.details
 import ca.tetervak.mathtrainer.domain.UserProblem
 
 data class ProblemDetailsUiState(
-    val userProblem: UserProblem,
-    val score: Int = 0,
+    val userProblem: UserProblem
 )
