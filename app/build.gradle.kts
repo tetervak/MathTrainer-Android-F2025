15f38f061ab5f83f4fe9ebff03b670261a981591
Alex Tetervak <tetervak@sheridancollege.ca>
2025-10-08 19:12:45 -0400

    rearranged build.gradle
    (cherry picked from commit 0675124d11dac5c13ee451d9b1bfd68dbe0b33e8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,18 +51,19 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-
-    implementation(libs.androidx.room.runtime)
-    implementation(libs.androidx.hilt.navigation.compose)
     implementation(libs.androidx.compose.material.icons.core)
     implementation(libs.androidx.compose.material.icons.extended)
-    implementation(libs.androidx.compose.material3.window.size)
+
+    implementation(libs.androidx.room.runtime)
     ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.room.ktx)
 
     implementation(libs.hilt.android)
     implementation(libs.androidx.hilt.lifecycle.viewmodel.compose)
     ksp(libs.hilt.android.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    implementation(libs.androidx.compose.material3.window.size)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
