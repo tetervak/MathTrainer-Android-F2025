0aab981833d617de314229fb196c5b10def9bbdb
Alex Tetervak <tetervak@sheridancollege.ca>
2025-10-31 10:12:05 -0400

    changed the min sdk to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "ca.tetervak.mathtrainer"
-        minSdk = 35
+        minSdk = 34
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
