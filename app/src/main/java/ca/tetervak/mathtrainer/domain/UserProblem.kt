07ed77fb02202790ea7e83d2a9ab8c5950ac72dc
Alex Tetervak <tetervak@sheridancollege.ca>
2025-10-31 18:53:54 -0400

    simplified check answer code

--- app/src/main/java/ca/tetervak/mathtrainer/domain/UserProblem.kt
+++ app/src/main/java/ca/tetervak/mathtrainer/domain/UserProblem.kt
@@ -9,20 +9,9 @@ class UserProblem(
     val text: String
         get() = problem.text
 
-    val status: Status = if (userAnswer != null) {
-        when (problem.checkAnswer(userAnswer = userAnswer)) {
-            AlgebraProblem.Grade.RIGHT_ANSWER -> Status.RIGHT_ANSWER
-            AlgebraProblem.Grade.WRONG_ANSWER -> Status.WRONG_ANSWER
-            AlgebraProblem.Grade.INVALID_INPUT -> Status.INVALID_INPUT
-        }
-    } else {
-        Status.NOT_ANSWERED
-    }
+    val status: UserAnswerStatus = problem.checkAnswer(userAnswer = userAnswer)
 
     fun copy(userAnswer: String? = this.userAnswer) =
         UserProblem(problem = problem, userAnswer = userAnswer, id = id)
 
-    enum class Status {
-        NOT_ANSWERED, RIGHT_ANSWER, WRONG_ANSWER, INVALID_INPUT
-    }
 }
--- app/src/test/java/ca/tetervak/mathtrainer/domain/UserProblemTest.kt
+++ app/src/test/java/ca/tetervak/mathtrainer/domain/UserProblemTest.kt
@@ -33,27 +33,27 @@ class UserProblemTest {
 
         val copy1 = userProblem.copy(userAnswer = "3")
         assertEquals("3", copy1.userAnswer)
-        assertEquals(UserProblem.Status.RIGHT_ANSWER, copy1.status)
+        assertEquals(UserAnswerStatus.RIGHT_ANSWER, copy1.status)
 
         val copy2 = userProblem.copy(userAnswer = "4")
         assertEquals("4", copy2.userAnswer)
-        assertEquals(UserProblem.Status.WRONG_ANSWER, copy2.status)
+        assertEquals(UserAnswerStatus.WRONG_ANSWER, copy2.status)
 
         val copy3 = userProblem.copy(userAnswer = "whatever")
         assertEquals("whatever", copy3.userAnswer)
-        assertEquals(UserProblem.Status.INVALID_INPUT, copy3.status)
+        assertEquals(UserAnswerStatus.INVALID_INPUT, copy3.status)
 
         val copy4 = userProblem.copy(userAnswer = "3.0")
         assertEquals("3.0", copy4.userAnswer)
-        assertEquals(UserProblem.Status.RIGHT_ANSWER, copy4.status)
+        assertEquals(UserAnswerStatus.RIGHT_ANSWER, copy4.status)
 
         val copy5 = userProblem.copy(userAnswer = "3.1")
         assertEquals("3.1", copy5.userAnswer)
-        assertEquals(UserProblem.Status.WRONG_ANSWER, copy5.status)
+        assertEquals(UserAnswerStatus.WRONG_ANSWER, copy5.status)
 
         val copy6 = userProblem.copy(userAnswer = "2.9")
         assertEquals("2.9", copy6.userAnswer)
-        assertEquals(UserProblem.Status.WRONG_ANSWER, copy6.status)
+        assertEquals(UserAnswerStatus.WRONG_ANSWER, copy6.status)
 
     }
 
