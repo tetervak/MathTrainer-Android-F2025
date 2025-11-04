02c0e9038521058f7eef168f4aac4d8723e1cfb4
Alex Tetervak <tetervak@sheridancollege.ca>
2025-11-04 10:16:02 -0500

    caught the server connection exceptions

--- app/src/main/java/ca/tetervak/mathtrainer/data/repository/RandomQuizRepository.kt
+++ app/src/main/java/ca/tetervak/mathtrainer/data/repository/RandomQuizRepository.kt
@@ -1,5 +1,6 @@
 package ca.tetervak.mathtrainer.data.repository
 
+import android.util.Log
 import ca.tetervak.mathtrainer.domain.AlgebraProblem
 import ca.tetervak.mathtrainer.domain.ProblemGeneration
 import kotlinx.coroutines.Dispatchers
@@ -12,13 +13,23 @@ class RandomQuizRepository @Inject constructor(
     private val userPreferencesRepository: UserPreferencesRepository
 ) {
 
+    companion object {
+        private const val TAG = "RandomQuizRepository"
+    }
+
     suspend fun getRandomQuizProblems(): List<AlgebraProblem> =
         withContext(context = Dispatchers.IO){
             val userPreferences = userPreferencesRepository.getUserPreferences()
             val numberOfProblems: Int = userPreferences.numberOfProblems
             when(userPreferences.problemGeneration){
                 ProblemGeneration.LOCAL -> localRandomQuizRepository.getRandomQuizProblems(numberOfProblems)
-                ProblemGeneration.REMOTE -> remoteRandomQuizRepository.getRandomQuizProblems(numberOfProblems)
+                ProblemGeneration.REMOTE -> try {
+                    remoteRandomQuizRepository.getRandomQuizProblems(numberOfProblems)
+                } catch (_: Exception){
+                    Log.w(TAG,
+                        "getRandomQuizProblems: Could not connect to the server, generating problems locally.")
+                    localRandomQuizRepository.getRandomQuizProblems(numberOfProblems)
+                }
             }
         }
 }
