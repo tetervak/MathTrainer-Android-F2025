23330c9bc8eb51d36e6bec9a3ebf16559099bfd3
Alex Tetervak <tetervak@sheridancollege.ca>
2025-11-13 12:52:54 -0500

    simplified FireStoreProblem

--- app/src/main/java/ca/tetervak/mathtrainer/data/firebase/FirestoreProblem.kt
+++ app/src/main/java/ca/tetervak/mathtrainer/data/firebase/FirestoreProblem.kt
@@ -7,9 +7,7 @@ import com.google.firebase.Timestamp
 data class FirestoreProblem(
     val id: String = "",
     val problemNumber: Int,
-    val firstNumber: Int,
-    val secondNumber: Int,
-    val algebraOperation: AlgebraOperation,
+    val text: String,
     val userAnswer: String? = null,
     val answerStatus: AnswerStatus = AnswerStatus.NOT_ANSWERED,
     val updatedAt: Timestamp? = null
@@ -19,10 +17,7 @@ data class FirestoreProblem(
 
 // the id is not in the map to avoid data duplication
 fun FirestoreProblem.toMap(): Map<String, Any?> = mapOf(
-    "problemNumber" to problemNumber,
-    "firstNumber" to firstNumber,
-    "secondNumber" to secondNumber,
-    "algebraOperation" to algebraOperation.name,
+    "text" to text,
     "userAnswer" to userAnswer,
     "answerStatus" to answerStatus.name,
     "updatedAt" to updatedAt
@@ -32,9 +27,7 @@ fun FirestoreProblem.Companion.fromMap(problemId: String, data: Map<String, Any>
     FirestoreProblem(
         id = problemId,
         problemNumber = (data["problemNumber"] as Long).toInt(),
-        firstNumber = (data["firstNumber"] as Long).toInt(),
-        secondNumber = (data["secondNumber"] as Long).toInt(),
-        algebraOperation = AlgebraOperation.valueOf(data["algebraOperation"] as String),
+        text = data["text"] as String,
         userAnswer = data["userAnswer"] as String?,
         answerStatus = AnswerStatus.valueOf(data["answerStatus"] as String),
         updatedAt = data["updatedAt"] as Timestamp?
