cc913534575ae195078ae405da921f1721772482
Alex Tetervak <tetervak@sheridancollege.ca>
2025-11-14 14:13:24 -0500

    removed the userId from the domain class Quiz

--- app/src/main/java/ca/tetervak/mathtrainer/domain/model/QuizStatus.kt
+++ app/src/main/java/ca/tetervak/mathtrainer/domain/model/QuizStatus.kt
@@ -9,4 +9,13 @@ data class QuizStatus(
     val invalidInputs: Int =
         problemCount - (rightAnswers + wrongAnswers + notAnswered)
     val answeredCount: Int = rightAnswers + wrongAnswers + invalidInputs
+
+    companion object {
+        val Preview: QuizStatus = QuizStatus(
+            problemCount = 15,
+            rightAnswers = 8,
+            wrongAnswers = 2,
+            notAnswered = 3,
+        )
+    }
 }
